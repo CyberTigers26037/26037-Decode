99760a357ea0ff9e611bacd69a23d2c9a3198f99
anpark1234 <anthony@anpark.com>
2025-10-19 12:27:52 -0700

    Added support for two color sensors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ColorSensorTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ColorSensorTestOpMode.java
@@ -10,27 +10,47 @@ import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 @SuppressWarnings("unused")
 @TeleOp(name = "ColorSensorTestOpMode", group = "Test")
 public class ColorSensorTestOpMode extends OpMode {
-    private NormalizedColorSensor colorSensor;
+    private NormalizedColorSensor colorSensor1;
+    private NormalizedColorSensor colorSensor2;
 
     @Override
     public void init() {
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class,"sensor_color_distance");
+        colorSensor1 = hardwareMap.get(NormalizedColorSensor.class,"sensor_color_distance_one");
+        colorSensor2 = hardwareMap.get(NormalizedColorSensor.class,"sensor_color_distance_two");
     }
 
     @Override
     public void loop() {
-        final float[] hsvValues = new float[3];
-        NormalizedRGBA colors = colorSensor.getNormalizedColors();
-        Color.colorToHSV(colors.toColor(), hsvValues);
+        final float[] hsvValues1 = new float[3];
+        NormalizedRGBA colors1 = colorSensor1.getNormalizedColors();
+        Color.colorToHSV(colors1.toColor(), hsvValues1);
 
-        telemetry.addData("Red", "%.3f", colors.red);
-        telemetry.addData("Green", "%.3f", colors.green);
-        telemetry.addData("Blue", "%.3f", colors.blue);
+        final float[] hsvValues2 = new float[3];
+        NormalizedRGBA colors2 = colorSensor2.getNormalizedColors();
+        Color.colorToHSV(colors2.toColor(), hsvValues2);
+
+        telemetry.addLine("Color Sensor 1");
+        telemetry.addLine()
+                .addData("Red", "%.3f", colors1.red)
+                .addData("Green", "%.3f", colors1.green)
+                .addData("Blue", "%.3f", colors1.blue);
+        telemetry.addLine()
+                .addData("Hue", "%.3f", hsvValues1[0])
+                .addData("Saturation", "%.3f", hsvValues1[1])
+                .addData("Value", "%.3f", hsvValues1[2]);
+        telemetry.addData("Alpha", "%.3f", colors1.alpha);
+
         telemetry.addLine();
-        telemetry.addData("Hue", "%.3f", hsvValues[0]);
-        telemetry.addData("Saturation", "%.3f", hsvValues[1]);
-        telemetry.addData("Value", "%.3f", hsvValues[2]);
-        telemetry.addLine();
-        telemetry.addData("Alpha", "%.3f", colors.alpha);
+
+        telemetry.addLine("Color Sensor 2");
+        telemetry.addLine()
+                .addData("Red", "%.3f", colors2.red)
+                .addData("Green", "%.3f", colors2.green)
+                .addData("Blue", "%.3f", colors2.blue);
+        telemetry.addLine()
+                .addData("Hue", "%.3f", hsvValues2[0])
+                .addData("Saturation", "%.3f", hsvValues2[1])
+                .addData("Value", "%.3f", hsvValues2[2]);
+        telemetry.addData("Alpha", "%.3f", colors2.alpha);
     }
 }

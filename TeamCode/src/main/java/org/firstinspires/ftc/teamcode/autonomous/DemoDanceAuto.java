302091eab327c6ceab7e56a1969ca8aa3fda73c4
MRLPx <127459781+MRLPx@users.noreply.github.com>
2026-03-30 16:25:57 -0700

    Worked on dance Auto some more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DemoDanceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DemoDanceAuto.java
@@ -5,6 +5,7 @@ import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.subassembly.ArtifactColor;
 
@@ -14,11 +15,7 @@ import org.firstinspires.ftc.teamcode.subassembly.ArtifactColor;
 @Autonomous(name= "DanceAuto", group="Pedro")
 public class DemoDanceAuto extends PedroAutoBase {
     public static class CloseAutoConfig {
-        public ConfigurablePose startPoseBlue = new ConfigurablePose(18, 119, 52);
-        public ConfigurablePose startPoseRed  = new ConfigurablePose(125, 119, 126);
-
-        public ConfigurablePose parkPoseBlue = new ConfigurablePose(30, 58, 180);
-        public ConfigurablePose parkPoseRed  = new ConfigurablePose(114, 45, 50);
+        public ConfigurablePose startPose = new ConfigurablePose(0, 0, 0);
 
     }
 
@@ -34,10 +31,14 @@ public class DemoDanceAuto extends PedroAutoBase {
     // Attempt launch
     // Park
 
+
     private enum PathState {
         SPIN,
         STOP_SPINNING,
-        CYCLE_LIGHT,
+        CYCLE_LIGHT1,
+        CYCLE_LIGHT2,
+        CYCLE_LIGHT3,
+        STOP_CYCLE_LIGHT,
         INTAKE,
         STOP_INTAKE,
         REVERSE_INTAKE,
@@ -47,90 +48,147 @@ public class DemoDanceAuto extends PedroAutoBase {
         CAROUSEL2,
         CAROUSEL3,
         RAISE_FLIPPER,
+        PARK_FLIPPER,
         LAUNCH,
         PARK,
         STOP
     }
 
     private PathState pathState;
+    private Servo rgbLight;
 
+    private static final double OFF = 0.0;
+    private static final double BLUE = 0.611;
+    private static final double RED = 0.279;
+
+    @Override
+    public void init() {
+        super.init();
+        rgbLight = hardwareMap.get(Servo.class, "artifactLight");
+    }
 
     public Pose getStartPose() {
-        // Start Pose of our robot.
-        return isBlueAlliance ? config.startPoseBlue.getPose() : config.startPoseRed.getPose();
+        return startPose.getPose();
     }
 
     /** We do not use this because everything should automatically disable **/
 
-    private PathChain park;
     public void buildPaths() {
-        Pose parkPose = isBlueAlliance ? config.parkPoseBlue.getPose() : config.parkPoseRed.getPose();
 
-        park = follower.pathBuilder()
     }
 
     public void autonomousPathUpdate() {
         switch (pathState) {
             case SPIN:
+                follower.turnDegrees(720, false);
                 setPathState(PathState.STOP_SPINNING);
                 break;
             case STOP_SPINNING:
-                setPathState(PathState.CYCLE_LIGHT);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 3.0)) {
+                    setPathState(PathState.CYCLE_LIGHT1);
+                }
                 break;
 
-            case CYCLE_LIGHT:
-                setPathState(PathState.INTAKE);
+            case CYCLE_LIGHT1:
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    rgbLight.setPosition(RED);
+                    setPathState(PathState.CYCLE_LIGHT2);
+                }
+                break;
+            case CYCLE_LIGHT2:
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    rgbLight.setPosition(BLUE);
+                    setPathState(PathState.CYCLE_LIGHT3);
+                }
+                break;
+            case CYCLE_LIGHT3:
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    rgbLight.setPosition(RED);
+                    setPathState(PathState.STOP_CYCLE_LIGHT);
+                }
+                break;
+            case STOP_CYCLE_LIGHT:
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    rgbLight.setPosition(OFF);
+                    setPathState(PathState.INTAKE);
+                }
                 break;
 
             case INTAKE:
-                artifactSystem.startIntake();
-                setPathState(PathState.STOP_INTAKE);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.startIntake();
+                    setPathState(PathState.STOP_INTAKE);
+                }
                 break;
             case STOP_INTAKE:
-                artifactSystem.stopIntake(false);
-                setPathState(PathState.REVERSE_INTAKE);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.stopIntake(false);
+                    setPathState(PathState.REVERSE_INTAKE);
+                }
                 break;
             case REVERSE_INTAKE:
-                artifactSystem.startReverseIntake();
-                setPathState(PathState.ADJUST_LAUNCHER_CLOSE);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.startReverseIntake();
+                    setPathState(PathState.ADJUST_LAUNCHER_CLOSE);
+                }
                 break;
 
             case ADJUST_LAUNCHER_CLOSE:
-                artifactSystem.stopReverseIntake();
-                adjustLauncherAngle.adjustCloseAngle();
-                setPathState(PathState.ADJUST_LAUNCHER_FAR);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.stopReverseIntake();
+                    adjustLauncherAngle.adjustCloseAngle();
+                    setPathState(PathState.ADJUST_LAUNCHER_FAR);
+                }
                 break;
             case ADJUST_LAUNCHER_FAR:
-                adjustLauncherAngle.adjustCloseAngle();
-                setPathState(PathState.CAROUSEL1);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    adjustLauncherAngle.adjustCloseAngle();
+                    setPathState(PathState.CAROUSEL1);
+                }
                 break;
 
             case CAROUSEL1:
-                artifactSystem.moveCarouselToPosition(1);
-                setPathState(PathState.CAROUSEL2);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.moveCarouselToPosition(1);
+                    setPathState(PathState.CAROUSEL2);
+                }
                 break;
             case CAROUSEL2:
                 artifactSystem.moveCarouselToPosition(2);
                 setPathState(PathState.CAROUSEL3);
                 break;
             case CAROUSEL3:
-                artifactSystem.moveCarouselToPosition(3);
-                setPathState(PathState.RAISE_FLIPPER);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.moveCarouselToPosition(3);
+                    setPathState(PathState.RAISE_FLIPPER);
+                }
                 break;
 
             case RAISE_FLIPPER:
-                artifactSystem.raiseFlipper();
-                setPathState(PathState.LAUNCH);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.raiseFlipper();
+                    setPathState(PathState.LAUNCH);
+                }
+                break;
+            case PARK_FLIPPER:
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.raiseFlipper();
+                    setPathState(PathState.LAUNCH);
+                }
                 break;
             case LAUNCH:
-                artifactSystem.setLauncherRpm(1000);
-                artifactSystem.startLauncher();
-                setPathState(PathState.PARK);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 0.5)) {
+                    artifactSystem.setLauncherRpm(1000);
+                    artifactSystem.startLauncher();
+                    setPathState(PathState.PARK);
+                }
                 break;
 
             case PARK:
-                artifactSystem.stopLauncher();
-                setPathState(PathState.STOP);
+                if ((!follower.isBusy()) || (pathTimer.getElapsedTimeSeconds() > 1.0)) {
+                    artifactSystem.stopLauncher();
+                    setPathState(PathState.STOP);
+                }
                 break;
 
         }
@@ -163,7 +221,7 @@ public class DemoDanceAuto extends PedroAutoBase {
     public void start() {
         super.start();
         opmodeTimer.resetTimer();
-        setPathState(PathState.PREP_SCAN_OBELISK);
+        setPathState(PathState.SPIN);
     }
 
     @Override
